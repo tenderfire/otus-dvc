c62cc6044d5a6f00e2ed12269d9e3904a93c33cf
Pavel Slugin <pavel.slugin@gmail.com>
2025-10-22 21:08:38 +0800

    fe at: Add invoke test

--- tasks.py
+++ tasks.py
@@ -1,7 +1,14 @@
 from invoke import task
+from dotenv import load_dotenv
+
+load_dotenv()
 
 @task
 def setup(ctx):
     """Set up the development environment by installing dependencies."""
     ctx.run("uv python install 3.11.12", pty=True)
     ctx.run("uv sync", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("echo $TEST_ENV", pty=True)
