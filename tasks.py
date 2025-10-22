6dfd31c33ed74969701eef4328f6240911b1a3e5
Pavel Slugin <pavel.slugin@gmail.com>
2025-10-22 21:25:24 +0800

    feat: Add invoke train stage

--- tasks.py
+++ tasks.py
@@ -17,3 +17,7 @@ def test(ctx):
 def setup_dvc(ctx):
     ctx.run("dvc init", pty=True)
     ctx.run("bash setup-dvc.sh", pty=True)
+
+@task
+def add_train_stage(ctx):
+    ctx.run("dvc stage add -n train -d train.py -d data -o models/model.joblib -M metrics.csv python3 train.py", pty=True)    
